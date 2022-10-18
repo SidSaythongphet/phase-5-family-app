ee314681e62554d7199d27a2dc6ded05ea841b2b
Sid Saythongphet <sid.saythongphet@gmail.com>
2022-10-18 12:50:16 -0400

    add react-hook-form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from "./components/navigation/NavBar";
+import SignUp from "./components/session/SignUp";
 import Home from "./components/static/Home";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={ <Home /> } />
+        <Route path="/signup" element={ <SignUp /> } />
       </Routes>
     </Router>
   );
--- client/src/components/session/SignUp.jsx
+++ client/src/components/session/SignUp.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { useForm } from "react-hook-form";
 
 const SignUp = () => {
+  const { register, handleSubmit } = useForm()
+  const onSubmit = data => console.log(data)
+
   return (
-    <div>SignUp</div>
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input placeholder="First Name"{...register("firstName", { required: true, maxLength: 20 })} />
+        <input placeholder="Last Name"{...register("lastName", { required: true, pattern: /^[A-Za-z]+$/i })} />
+        <input placeholder="Password"{...register("password", { required: true, maxLength: 20 })} />
+        <input type="submit" />
+      </form>
+    </div>
   )
 }
 
