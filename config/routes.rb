44f672ea8031b81b60ee00023e6366c0808eefde
Sid Saythongphet <sid.saythongphet@gmail.com>
2022-10-18 13:55:05 -0400

    add family resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  scope :api do
+    resources :families, only: [:index, :show, :create]
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
