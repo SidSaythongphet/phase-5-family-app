f5d8375201477433a3402e5f2aa6a563a4b4f1cc
Sid Saythongphet <sid.saythongphet@gmail.com>
2022-11-08 17:08:49 -0500

    add user color theme to navbar

--- client/src/components/navigation/NavBar.jsx
+++ client/src/components/navigation/NavBar.jsx
@@ -4,24 +4,39 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import { useNavigate } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import  ButtonGroup from '@mui/material/ButtonGroup';
 
 const NavBar = ({ onOpenUsers, user, loggedIn, onLogout }) => {
-  const navigate = useNavigate()
+  if (!user) return null
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: user.color,
+      },
+    },
+  })
 
   return (
+    <ThemeProvider theme={theme}>
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" sx={{ bgcolor: 'primary.dark' }}>
         <Toolbar>
+          <Box sx={{ marginRight: "19vw"}}>
+            <Typography variant="h4">Plannr</Typography>
+          </Box>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            { loggedIn ? "Welcome" : "Please Log In"}
+            { loggedIn ? "Hello, " : null}
             { user ? user.name : null }
           </Typography>
             { loggedIn 
               ?
               <>
-                <Button color="inherit" onClick={ onOpenUsers }>Switch User</Button>
-                <Button color="inherit" onClick={ onLogout }>Logout</Button>
+                <ButtonGroup>
+                  <Button variant="contained" color="primary" onClick={ onOpenUsers }>Switch User</Button>
+                  <Button variant="contained" color="primary" onClick={ onLogout }>Logout</Button>
+                </ButtonGroup>
               </> 
               :
               null
@@ -29,6 +44,7 @@ const NavBar = ({ onOpenUsers, user, loggedIn, onLogout }) => {
         </Toolbar>
       </AppBar>
     </Box>
+    </ThemeProvider>
   );
 }
 
