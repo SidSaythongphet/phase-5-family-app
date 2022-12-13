869db1a173068ec9f9df47c6dadce73d9bea6835
Sid Saythongphet <sid.saythongphet@gmail.com>
2022-12-13 14:05:35 -0500

    change namescape to scope in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  namespace :api do
+  scope :api do
     post "/signup", to: "families#create"
     get "/family", to: "families#show"
     delete "/logout", to: "sessions#destroy"
